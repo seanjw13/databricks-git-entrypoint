46fa2dd1604b5d493fb24b53aec8de34c60413d8
seanjw13 <seanjw13>
2023-06-05 14:49:12 +0000

    Parameterize the notebook

--- Notebook1.py
+++ Notebook1.py
@@ -1,10 +1,14 @@
 # Databricks notebook source
+dbutils.widgets.text("Input", "Notebook2")
+
+# COMMAND ----------
+
+notebook = dbutils.widgets.get("Input")
+
+# COMMAND ----------
+
 print("Hello notebook1")
 
 # COMMAND ----------
 
-dbutils.notebook.run("Notebook2", 60)
-
-# COMMAND ----------
-
-
+dbutils.notebook.run(notebook, 60)
